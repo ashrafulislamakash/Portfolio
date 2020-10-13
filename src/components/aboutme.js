c7036a2d3a99b5b1d39973e567699d7cb517c04f
Md Ashraful Islam <71764995+ashrafulislamakash@users.noreply.github.com>
2020-10-13 23:30:25 +0600

    About me

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -5,11 +5,7 @@ class About extends Component {
   render() {
     return(
       <div>
-         <img
-              src="ashraful.jpg"
-              alt="Md Ashraful Islam"
-              style={{height: '250px'}}
-               />
+        
       
       </div>
     )
